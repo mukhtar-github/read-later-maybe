892f1b855bccde092f7eceedb0641d1dab285bec
Mukhtar <mukhtartanimugarba@gmail.com>
2023-05-13 06:57:08 +0100

    Update changes

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginView } from "./views/LoginView";
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
